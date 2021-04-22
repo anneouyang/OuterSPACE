fc21c7ae1eec65a3a697cd20863f79a458973f10
Anne Ouyang <anne.ouyang@gmail.com>
2021-04-22 02:28:59 -0400

    Add sparse utils

--- NN_models/MLP1/sparse_test.py
+++ NN_models/MLP1/sparse_test.py
@@ -1,10 +1,18 @@
+import torch
+
 from models import MLP1
 from main import save_model_weights, load_model_weights
+from sparse_util import *
 
 save_dir = 'saved_weights/test1/'
 model = MLP1()
 load_model_weights(model, save_dir)
-# print(model)
 
-for param in model.parameters():
-	print(param.data)
+
+# for param in model.parameters():
+# 	print(get_sparsity(param))
+
+# mat = model.fc1.weight.data
+# mask = get_sparse_mask(mat, 0.01)
+# print(mask)
+# print(mat * mask)
--- NN_models/MLP1/sparse_util.py
+++ NN_models/MLP1/sparse_util.py
@@ -1,9 +1,21 @@
+import torch 
+
+# sparsity level is defined as number of non-zero elements / total size
+
 def get_sparsity(mat):
-	pass
+	zeros_count = (abs(mat - 0) <= 0.000001).sum()
+	return (zeros_count, torch.numel(mat), 1 - zeros_count / torch.numel(mat))
+
+
+def get_sparse_mask(mat, sparsity_level):
+	threshold = torch.quantile(abs(mat), 1 - sparsity_level)
+	mask = (mat > threshold)
+	return mask
 
 
 def prune_to_sparsity(mat, sparsity_level):
 	# if already at or below desired sparsity level
 	if get_sparsity(mat) <= sparsity_level:
 		return mat
-
+	mask = get_sparse_mask(mat, sparsity_level)
+	return (mask, mat * mask)
